6929cd34612bda5c2564af0fbfe714f9836f35f6
Elijah Smith <elijahsmith.m@gmail.com>
2022-03-15 11:54:17 -0500

    commit tree 9838fa8a4fe1c74d05ef937e9b747df8c5f0571d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+// let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+// let newArray;
+
+const returnFirstTwoDrivers = drivers => drivers.slice(0,2);
+
+const returnLastTwoDrivers = drivers => drivers.slice(-2);
+
+const selectingDrivers = [
+    returnFirstTwoDrivers, returnLastTwoDrivers
+];
+
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+        return integer * fare;
+    }
+} 
+
+const fareDoubler = createFareMultiplier(2); // 2 here is integer from createFareMultiplier
+
+const fareTripler = createFareMultiplier(3);
+
+selectDifferentDrivers = (drivers, returnLastTwoDrivers) => returnLastTwoDrivers(drivers);
+
